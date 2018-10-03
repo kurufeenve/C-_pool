bf929c6d6fe6dc13419be9386f14023d727a5f33
Vladyslav ORDYNSKYI <vordynsk@e3r7p8.unit.ua>
2018-10-03 13:47:34 +0300

    ex07

--- day01/ex07/main.cpp
+++ day01/ex07/main.cpp
@@ -10,4 +10,27 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "Sed.class.hpp"
 
+int		main(int argc, char **argv)
+{
+	Sed				*sed;
+	
+	if (argc != 4)
+	{
+		std::cout << "wrong number of arguments." << std::endl;
+		exit(0);
+	}
+	try
+	{
+		sed = new Sed(argv[1], argv[2], argv[3]);
+	}
+	catch(int e) {
+		exit(0);
+	}
+	delete sed;
+	return (0);
+}
