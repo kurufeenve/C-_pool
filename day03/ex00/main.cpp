f980ff9fbfcabc49f4f2f45fb6612fdc89444591
Vladyslav ORDYNSKYI <vordynsk@e3r7p8.unit.ua>
2018-10-04 22:25:56 +0300

    day03/ex00

---
+++ day03/ex00/main.cpp
@@ -0,0 +1,20 @@
+#include "FragTrap.hpp"
+
+int		main(void)
+{
+	FragTrap	a = FragTrap("R2D2");
+
+	a.rangedAttack("bobby");
+	a.takeDamage(50);
+	a.meleeAttack("bobby");
+	a.takeDamage(20);
+	a.rageOfGandi("Bobby");
+	a.beRepaired(60);
+	a.vaulthunter_dot_exe("Bobby");
+	a.vaulthunter_dot_exe("Bobby");
+	a.vaulthunter_dot_exe("Bobby");
+	a.vaulthunter_dot_exe("Bobby");
+	a.vaulthunter_dot_exe("Bobby");
+	a.vaulthunter_dot_exe("Bobby");
+	return (0);
+}
